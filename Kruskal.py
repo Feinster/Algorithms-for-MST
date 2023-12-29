42b43a68a84a61ae805c4cb3335316a92d6e9074
Matteo Fenu <46670630+Feinster@users.noreply.github.com>
2023-12-29 14:51:46 +0100

    work in progress

--- Kruskal.py
+++ Kruskal.py
@@ -1,37 +1,43 @@
 class Graph:
     def __init__(self, num_of_nodes):
+        # Initialization of the Graph class with the number of nodes and an empty graph
         self.m_num_of_nodes = num_of_nodes
         self.m_graph = []
 
     def add_edge(self, node1, node2, weight):
+        # Method to add an edge to the graph with specified nodes and weight
         self.m_graph.append([node1, node2, weight])
         
-    # Finds the root node of a subtree containing node `i`
+    # Finds the root node of a subtree containing node `i` using the path compression technique
     def find_subtree(self, parent, i):
         if parent[i] == i:
             return i
-        return self.find_subtree(parent, parent[i])
+        # Path compression: set the parent of `i` to the root of the subtree
+        parent[i] = self.find_subtree(parent, parent[i])
+        return parent[i]
 
-    # Connects subtrees containing nodes `x` and `y`
+    # Connects subtrees containing nodes `x` and `y` based on their sizes
     def connect_subtrees(self, parent, subtree_sizes, x, y):
         xroot = self.find_subtree(parent, x)
         yroot = self.find_subtree(parent, y)
+        # Union by rank: attach the smaller subtree to the root of the larger subtree
         if subtree_sizes[xroot] < subtree_sizes[yroot]:
             parent[xroot] = yroot
         elif subtree_sizes[xroot] > subtree_sizes[yroot]:
             parent[yroot] = xroot
         else:
+            # If both subtrees are of the same size, choose one as the root and increment its size
             parent[yroot] = xroot
             subtree_sizes[xroot] += 1
             
     def kruskals_mst(self):
-        # Resulting tree
+        # List to store the edges of the Minimum Spanning Tree (MST)
         result = []
         
         # Iterator
         i = 0
-        # Number of edges in MST
-        e = 0
+        # Number of edges in the MST
+        edge_count  = 0
 
         # Sort edges by their weight
         self.m_graph = sorted(self.m_graph, key=lambda item: item[2])
@@ -40,15 +46,16 @@ class Graph:
         parent = []
         subtree_sizes = []
 
-        # Initialize `parent` and `subtree_sizes` arrays
+        # Initialize `parent` and `subtree_sizes` arrays for union-find operations
         for node in range(self.m_num_of_nodes):
             parent.append(node)
             subtree_sizes.append(0)
 
-        # Important property of MSTs
-        # number of egdes in a MST is 
-        # equal to (m_num_of_nodes - 1)
-        while e < (self.m_num_of_nodes - 1):
+        # Important property of MSTs:
+        # the number of edges in an MST is equal to (m_num_of_nodes - 1)
+        # while we haven't yet added each edge
+        # increment iterator and run the union find algorithm
+        while edge_count  < (self.m_num_of_nodes - 1):
             # Pick an edge with the minimal weight
             node1, node2, weight = self.m_graph[i]
             i = i + 1
@@ -56,17 +63,22 @@ class Graph:
             x = self.find_subtree(parent, node1)
             y = self.find_subtree(parent, node2)
 
+            # If nodes don't belong to the same subtree, add the edge to the MST
             if x != y:
-                e = e + 1
+                edge_count = edge_count  + 1
                 result.append([node1, node2, weight])
                 self.connect_subtrees(parent, subtree_sizes, x, y)
         
-        # Print the resulting MST
+        # Print the resulting MST and its total weight
+        total_weight = 0
         for node1, node2, weight in result:
-            print("%d - %d: %d" % (node1, node2, weight))
+            print("({}, {}) cost: {}".format(node1, node2, weight))
+            total_weight += weight
+
+        print("\nTotal Weight of MST:", total_weight)
             
 def main():
-    # Example graph has 9 nodes
+    # Example graph with 9 nodes
     example_graph = Graph(9)
     example_graph.add_edge(0, 1, 4)
     example_graph.add_edge(0, 2, 7)
@@ -84,8 +96,15 @@ def main():
     example_graph.add_edge(6, 8, 5)
     example_graph.add_edge(7, 8, 12)
     
+    # Print the initial graph
+    print("\nInitial Graph:")
+    for edge in example_graph.m_graph:
+        print("({}, {}) cost: {}".format(edge[0], edge[1], edge[2]))
+        
+    # Find the MST and print the results
+    print("\MST:\n")
     example_graph.kruskals_mst()
 
-# Run the main function
+# Run the main function if the script is executed directly
 if __name__ == "__main__":
     main()
--- Prim.py
+++ Prim.py
@@ -6,30 +6,35 @@ class Graph:
                     for row in range(num_of_nodes)]
 
     def add_edge(self, node1, node2, weight):
+        # Add an edge to the graph with the given weight
         self.m_graph[node1][node2] = weight
         self.m_graph[node2][node1] = weight
+
     def prims_mst(self):
         # Defining a really big number, that'll always be the highest weight in comparisons
-        postitive_inf = float('inf')
+        positive_inf = float('inf')
 
         # This is a list showing which nodes are already selected 
-        # so we don't pick the same node twice and we can actually know when stop looking
+        # so we don't pick the same node twice and we can actually know when to stop looking
         selected_nodes = [False for node in range(self.m_num_of_nodes)]
 
         # Matrix of the resulting MST
         result = [[0 for column in range(self.m_num_of_nodes)] 
                     for row in range(self.m_num_of_nodes)]
-        
+
+        # Index to keep track of iterations
         indx = 0
+
+        # Print the initial graph for reference
         for i in range(self.m_num_of_nodes):
             print(self.m_graph[i])
-        
+
         print(selected_nodes)
 
         # While there are nodes that are not included in the MST, keep looking:
         while(False in selected_nodes):
             # We use the big number we created before as the possible minimum weight
-            minimum = postitive_inf
+            minimum = positive_inf
 
             # The starting node
             start = 0
@@ -38,39 +43,45 @@ class Graph:
             end = 0
 
             for i in range(self.m_num_of_nodes):
-                # If the node is part of the MST, look its relationships
+                # If the node is part of the MST, look at its relationships
                 if selected_nodes[i]:
                     for j in range(self.m_num_of_nodes):
-                        # If the analyzed node have a path to the ending node AND its not included in the MST (to avoid cycles)
-                        if (not selected_nodes[j] and self.m_graph[i][j]>0):  
-                            # If the weight path analized is less than the minimum of the MST
+                        # If the analyzed node has a path to the ending node AND it's not included in the MST (to avoid cycles)
+                        if (not selected_nodes[j] and self.m_graph[i][j] > 0):  
+                            # If the weight path analyzed is less than the minimum of the MST
                             if self.m_graph[i][j] < minimum:
                                 # Defines the new minimum weight, the starting vertex and the ending vertex
                                 minimum = self.m_graph[i][j]
                                 start, end = i, j
-            
+
             # Since we added the ending vertex to the MST, it's already selected:
             selected_nodes[end] = True
 
-            # Filling the MST Adjacency Matrix fields:
+            # Filling the MST adjacency matrix fields:
             result[start][end] = minimum
-            
-            if minimum == postitive_inf:
+
+            if minimum == positive_inf:
                 result[start][end] = 0
 
-            print("(%d.) %d - %d: %d" % (indx, start, end, result[start][end]))
+            # Print the edge that is added to the MST
+            print("adding edge %d. (%d - %d): cost: %d" % (indx, start, end, result[start][end]))
             indx += 1
-            
+
+            # Make the MST undirected by adding the reverse edge
             result[end][start] = result[start][end]
 
+        # Print the resulting MST and its total weight
+        total_weight = 0
+        
         # Print the resulting MST
-        # for node1, node2, weight in result:
         for i in range(len(result)):
             for j in range(0+i, len(result)):
                 if result[i][j] != 0:
-                    print("%d - %d: %d" % (i, j, result[i][j]))
+                    print("({}, {}) cost: {}".format(i, j, result[i][j]))
+                    total_weight += result[i][j]
+        
+        print("\nTotal Weight of MST:", total_weight)
 
-            
 def main():
     # Example graph has 9 nodes
     example_graph = Graph(9)
@@ -89,7 +100,8 @@ def main():
     example_graph.add_edge(5, 7, 3)
     example_graph.add_edge(6, 8, 5)
     example_graph.add_edge(7, 8, 12)
-    
+
+    # Run Prim's algorithm and print the resulting MST
     example_graph.prims_mst()
 
 # Run the main function
