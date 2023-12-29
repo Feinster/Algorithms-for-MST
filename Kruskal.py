bc86b24bff58ea0bcea2e6c63411a8689c8cee3b
Feinster <46670630+Feinster@users.noreply.github.com>
2023-12-29 17:51:31 +0100

    improvements

--- Kruskal.py
+++ Kruskal.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 class Graph:
     def __init__(self, num_of_nodes):
         # Initialization of the Graph class with the number of nodes and an empty graph
@@ -30,7 +33,15 @@ class Graph:
             parent[yroot] = xroot
             subtree_sizes[xroot] += 1
             
-    def kruskals_mst(self):
+    def kruskals_mst(self, debug=False):
+    
+        if debug:
+            #print the initial graph
+            print("\nInitial Graph:")
+            for edge in self.m_graph:
+                print("({}, {}) cost: {}".format(edge[0], edge[1], edge[2]))
+                
+        start_time = time.time()  
         # List to store the edges of the Minimum Spanning Tree (MST)
         result = []
         
@@ -71,39 +82,51 @@ class Graph:
         
         # Print the resulting MST and its total weight
         total_weight = 0
-        for node1, node2, weight in result:
-            print("({}, {}) cost: {}".format(node1, node2, weight))
-            total_weight += weight
+        if debug:
+            print("\nMST:\n")
+            for node1, node2, weight in result:
+                print("({}, {}) cost: {}".format(node1, node2, weight))
+                total_weight += weight
 
-        print("\nTotal Weight of MST:", total_weight)
+            print("\nTotal Weight of MST:", total_weight)
+        
+        end_time = time.time()  
+        execution_time = end_time - start_time 
+        print("\nExecution time with {} nodes: {}".format(self.m_num_of_nodes, execution_time))
             
 def main():
     # Example graph with 9 nodes
-    example_graph = Graph(9)
-    example_graph.add_edge(0, 1, 4)
-    example_graph.add_edge(0, 2, 7)
-    example_graph.add_edge(1, 2, 11)
-    example_graph.add_edge(1, 3, 9)
-    example_graph.add_edge(1, 5, 20)
-    example_graph.add_edge(2, 5, 1)
-    example_graph.add_edge(3, 6, 6)
-    example_graph.add_edge(3, 4, 2)
-    example_graph.add_edge(4, 6, 10)
-    example_graph.add_edge(4, 8, 15)
-    example_graph.add_edge(4, 7, 5)
-    example_graph.add_edge(4, 5, 1)
-    example_graph.add_edge(5, 7, 3)
-    example_graph.add_edge(6, 8, 5)
-    example_graph.add_edge(7, 8, 12)
-    
-    # Print the initial graph
-    print("\nInitial Graph:")
-    for edge in example_graph.m_graph:
-        print("({}, {}) cost: {}".format(edge[0], edge[1], edge[2]))
+    graph = Graph(9)
+    graph.add_edge(0, 1, 4)
+    graph.add_edge(0, 2, 7)
+    graph.add_edge(1, 2, 11)
+    graph.add_edge(1, 3, 9)
+    graph.add_edge(1, 5, 20)
+    graph.add_edge(2, 5, 1)
+    graph.add_edge(3, 6, 6)
+    graph.add_edge(3, 4, 2)
+    graph.add_edge(4, 6, 10)
+    graph.add_edge(4, 8, 15)
+    graph.add_edge(4, 7, 5)
+    graph.add_edge(4, 5, 1)
+    graph.add_edge(5, 7, 3)
+    graph.add_edge(6, 8, 5)
+    graph.add_edge(7, 8, 12)
         
-    # Find the MST and print the results
-    print("\MST:\n")
-    example_graph.kruskals_mst()
+    # Find the MST and #print the results
+    graph.kruskals_mst(debug=True)
+    
+    comparative_test=False
+    
+    if comparative_test:
+        for num_of_nodes in range(5, 1000, 100):
+            graph = Graph(num_of_nodes)
+            
+            for i in range(num_of_nodes - 1):
+                random_weight = random.randint(1, 10)  
+                graph.add_edge(i, i + 1, random_weight)
+        
+            graph.kruskals_mst()
 
 # Run the main function if the script is executed directly
 if __name__ == "__main__":
--- Prim.py
+++ Prim.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 class Graph:
     def __init__(self, num_of_nodes):
         self.m_num_of_nodes = num_of_nodes
@@ -10,7 +13,10 @@ class Graph:
         self.m_graph[node1][node2] = weight
         self.m_graph[node2][node1] = weight
 
-    def prims_mst(self):
+    def prims_mst(self, debug=False):
+        
+        start_time = time.time()  
+        
         # Defining a really big number, that'll always be the highest weight in comparisons
         positive_inf = float('inf')
 
@@ -26,10 +32,11 @@ class Graph:
         indx = 0
 
         # Print the initial graph for reference
-        for i in range(self.m_num_of_nodes):
-            print(self.m_graph[i])
-
-        print(selected_nodes)
+        if debug:
+            print("Initial graph (adjacency matrix):\n")
+            for i in range(self.m_num_of_nodes):
+                print(self.m_graph[i])
+            print("\n")
 
         # While there are nodes that are not included in the MST, keep looking:
         while(False in selected_nodes):
@@ -64,7 +71,8 @@ class Graph:
                 result[start][end] = 0
 
             # Print the edge that is added to the MST
-            print("adding edge %d. (%d - %d): cost: %d" % (indx, start, end, result[start][end]))
+            if debug:
+                print("adding edge %d. (%d - %d): cost: %d" % (indx, start, end, result[start][end]))
             indx += 1
 
             # Make the MST undirected by adding the reverse edge
@@ -74,35 +82,53 @@ class Graph:
         total_weight = 0
         
         # Print the resulting MST
-        for i in range(len(result)):
-            for j in range(0+i, len(result)):
-                if result[i][j] != 0:
-                    print("({}, {}) cost: {}".format(i, j, result[i][j]))
-                    total_weight += result[i][j]
+        if debug:
+            print("\nMST:\n")
+            for i in range(len(result)):
+                for j in range(0+i, len(result)):
+                    if result[i][j] != 0:
+                        print("({}, {}) cost: {}".format(i, j, result[i][j]))
+                        total_weight += result[i][j]
         
-        print("\nTotal Weight of MST:", total_weight)
+            print("\nTotal Weight of MST:", total_weight)
+        
+        end_time = time.time()  
+        execution_time = end_time - start_time 
+        print("\nExecution time with {} nodes: {}".format(self.m_num_of_nodes, execution_time))
 
 def main():
     # Example graph has 9 nodes
-    example_graph = Graph(9)
-    example_graph.add_edge(0, 1, 4)
-    example_graph.add_edge(0, 2, 7)
-    example_graph.add_edge(1, 2, 11)
-    example_graph.add_edge(1, 3, 9)
-    example_graph.add_edge(1, 5, 20)
-    example_graph.add_edge(2, 5, 1)
-    example_graph.add_edge(3, 6, 6)
-    example_graph.add_edge(3, 4, 2)
-    example_graph.add_edge(4, 6, 10)
-    example_graph.add_edge(4, 8, 15)
-    example_graph.add_edge(4, 7, 5)
-    example_graph.add_edge(4, 5, 1)
-    example_graph.add_edge(5, 7, 3)
-    example_graph.add_edge(6, 8, 5)
-    example_graph.add_edge(7, 8, 12)
-
+    graph = Graph(9)
+    graph.add_edge(0, 1, 4)
+    graph.add_edge(0, 2, 7)
+    graph.add_edge(1, 2, 11)
+    graph.add_edge(1, 3, 9)
+    graph.add_edge(1, 5, 20)
+    graph.add_edge(2, 5, 1)
+    graph.add_edge(3, 6, 6)
+    graph.add_edge(3, 4, 2)
+    graph.add_edge(4, 6, 10)
+    graph.add_edge(4, 8, 15)
+    graph.add_edge(4, 7, 5)
+    graph.add_edge(4, 5, 1)
+    graph.add_edge(5, 7, 3)
+    graph.add_edge(6, 8, 5)
+    graph.add_edge(7, 8, 12)
+    
     # Run Prim's algorithm and print the resulting MST
-    example_graph.prims_mst()
+    graph.prims_mst(debug=True)
+    
+    comparative_test=False
+    
+    if comparative_test:
+        for num_of_nodes in range(5, 1000, 100):
+            graph = Graph(num_of_nodes)
+
+            for i in range(num_of_nodes - 1):
+                random_weight = random.randint(1, 10)  
+                graph.add_edge(i, i + 1, random_weight)
+
+            graph.prims_mst()
 
 # Run the main function
 if __name__ == "__main__":
